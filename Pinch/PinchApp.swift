3538d4d74f5373d300ed4373792bccc19a208861
Volodymyr Mendyk <volodymyrmendyk70@gmail.com>
2023-03-30 16:36:29 +0200

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Volodymyr Mendyk on 30/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
